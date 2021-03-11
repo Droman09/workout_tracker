54d8db36cc55778557658538da401f38385aec47
David Roman <david_roman09@yahoo.com>
2021-03-11 17:10:52 -0600

    api routes for workouts and range

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,58 @@
 const Workout = require("../models/workout.js");
 
-module.exports =  function(app){
+module.exports = function (app) {
     app.get("/api/workouts", (req, res) => {
         Workout.find({})
-        .then(dbWorkout => {
-            res.json(dbWorkout)
-        })
-        .catch(err => {
-            res.json(err)
-        })
+            .then(dbWorkout => {
+                res.json(dbWorkout)
+            })
+            .catch(err => {
+                res.json(err)
+            })
+    })
+
+    app.get("/api/workouts/range", (req, res) => {
+        Workout.find({})
+            .then(dbWorkout => {
+                res.json(dbWorkout)
+            })
+            .catch(err => {
+                res.json(err)
+            })
     })
 
 
-    
+    app.post("/api/workouts/range", (req, res) => {
+        Workout.create({})
+            .then(dbWorkout => {
+                res.json(dbWorkout)
+            })
+            .catch(err => {
+                res.json(err)
+            })
+    })
+
+    app.post("/api/workouts", (req, res) => {
+        Workout.create({})
+            .then(dbWorkout => {
+                res.json(dbWorkout)
+            })
+            .catch(err => {
+                res.json(err)
+            })
+    })
+
+    app.put("/api/workouts/:id", (req, res) => {
+        Workout.findByIdAndUpdate(req.params.id, 
+        {$push: {exercises: req.body}},
+        {new: true })
+            .then(dbWorkout => {
+                res.json(dbWorkout)
+            })
+            .catch(err => {
+                res.json(err)
+            })
+    })
+
 
 }
